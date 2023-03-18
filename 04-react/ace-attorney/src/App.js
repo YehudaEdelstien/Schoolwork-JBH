d9b251d62cf17738abde011d2df8f6cb78ffebe8
Yehuda Edelstein <ystein1957@gmail.com>
2023-03-18 21:58:01 +0200

    new project - visual novel

---
+++ 04-react/ace-attorney/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import GameWindow from './Components/GameWindow/GameWindow.js';
+
+function App() {
+  return (
+    <div className="App">
+      <GameWindow />
+    </div>
+  );
+}
+
+export default App;
---
+++ 04-react/ace-attorney/src/Components/GameWindow/GameWindow.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function GameWindow() {
+    return <div id='game-window'></div>
+}
+
+export default GameWindow;
