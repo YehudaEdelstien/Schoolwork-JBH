35592dcee00ed6f521253fff402ae0cba257ffb9
Yehuda Edelstein <ystein1957@gmail.com>
2023-03-19 10:50:57 +0200

    added loading text file

--- 04-react/ace-attorney/src/Components/GameWindow/GameWindow.js
+++ 04-react/ace-attorney/src/Components/GameWindow/GameWindow.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import story1 from "../../StoryScripts/Story1.txt";
 
-function GameWindow() {
-    return <div id='game-window'></div>
+const storyURL = "../../StoryScripts/Story1.txt";
+
+export default function GameWindow() {
+    return <div id='game-window'>Story:{getStory()}</div>
 }
 
-export default GameWindow;
+function getStory() {
+    const story = fetch(story1)
+        .then(x => x.text())
+        .then(text => console.log(text));
+    return null;
+}
