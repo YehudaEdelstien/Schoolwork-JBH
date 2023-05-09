1205264395a53c164f5e12dca83340445ee23fb5
YehudaEdelstien <72869192+YehudaEdelstien@users.noreply.github.com>
2023-05-09 15:41:39 +0300

    lifted state to enable sorting by completion

--- 05-react/project/src/components/dashboard/pages/ToDos.jsx
+++ 05-react/project/src/components/dashboard/pages/ToDos.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import baseUrl from '../../baseUrl';
 
-function ToDos({userId}) {
+function ToDos({ userId }) {
     const [todos, setTodos] = useState();
-    const [todoElements, setTodoElements] = useState()
 
     useEffect(() => {
         getToDos();
@@ -11,18 +10,35 @@ function ToDos({userId}) {
 
     async function getToDos() {
         const toDoData = await baseUrl.get(`todos?userId=${userId}`)
-        setTodos(toDoData.data)
-        const toDoArray = toDoData.data.map((task, index) => ({...task, index: index}))
-        setTodoElements(toDoArray)
+        const toDoArray = toDoData.data.map((task, index) => ({ ...task, index: index }))
+        setTodos(toDoArray)
     }
 
-    function updateToDo(id) {
+    async function updateToDo(elemntObj) {
+        const indexToChange = todos.indexOf(elemntObj);
+        const prevCompletionState = elemntObj.completed;
 
+        setState({...elemntObj, completed: "changing"});
+
+        try {
+            await baseUrl.patch(`todos/${elemntObj.id}`, { completed: !prevCompletionState })
+            setState({...elemntObj, completed: !prevCompletionState})
+        } catch (e) {
+            console.error(e)
+        }
+
+        function setState(obj) {
+            setTodos(prev => {
+                const newState = [...prev];
+                prev[indexToChange] = obj;
+                return newState;
+            })
+        }
     }
 
     function sortTodos(sortMethod) {
         setTodos((prevTodos) => {
-            switch(sortMethod){
+            switch (sortMethod) {
                 case "id":
                     return prevTodos.toSorted((a, b) => a.id - b.id)
                 case "completed":
@@ -36,9 +52,9 @@ function ToDos({userId}) {
     return (
         <>
             <h3>TODOS</h3>
-            <SortSelect sortTodos={sortTodos}/>
-            {todoElements ?
-                todoElements.map(task => <ToDoItem task={task} key={task.id} />)
+            <SortSelect sortTodos={sortTodos} />
+            {todos ?
+                todos.map(task => <ToDoItem task={task} key={task.id} onChange={updateToDo}/>)
                 : <div className='Spinner'></div>}
         </>
     );
@@ -46,9 +62,9 @@ function ToDos({userId}) {
 
 export default ToDos;
 
-function SortSelect({sortTodos}) {
+function SortSelect({ sortTodos }) {
 
-    function setSelection({target}) {
+    function setSelection({ target }) {
         console.log(target.value)
         sortTodos(target.value)
     }
@@ -66,27 +82,27 @@ function SortSelect({sortTodos}) {
     )
 }
 
-function ToDoItem({ task }) {
-    const [completed, setIsCompleted] = useState(task.completed)
+function ToDoItem({ task, onChange }) {
+    //     const [completed, setIsCompleted] = useState(task.completed)
 
-    async function onChange() {
-        const prevState = completed
-        setIsCompleted("changing")
-        try {
-            await baseUrl.patch(`todos/${task.id}`, { completed: !prevState })
-            setIsCompleted(!prevState)
-        } catch (e) {
-            console.error(e)
-            setIsCompleted(prevState)
-        }
-    }
+    //     async function onChange() {
+    //         const prevState = completed
+    //         setIsCompleted("changing")
+    //         try {
+    //             await baseUrl.patch(`todos/${task.id}`, { completed: !prevState })
+    //             setIsCompleted(!prevState)
+    //         } catch (e) {
+    //             console.error(e)
+    //             setIsCompleted(prevState)
+    //         }
+    //     }
 
     return (
         <div>
-            <span>{task.index}. </span>
+            <span>{task.index + 1}. </span>
             {task.title}
-            {completed !== "changing" ?
-                <input type='checkbox' checked={completed} onChange={onChange} />
+            {task.completed !== "changing" ?
+                <input type='checkbox' checked={task.completed} onChange={() => onChange(task)} />
                 : <div className='Spinner Spinner-sml'></div>}
         </div>
     )
