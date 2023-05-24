6799c76c36a62ae22787c779c447c1d15aa5f959
YehudaEdelstien <72869192+YehudaEdelstien@users.noreply.github.com>
2023-05-24 15:34:59 +0300

    Loops - problem 9

---
+++ Java/Homework/src/Loops/L9.java
@@ -0,0 +1,27 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class L9 {
+    public static void main(String[] args) {
+        int num = getNumberInput("Enter a number:");
+        System.out.println(num+" has "+divisors(num)+" divisors");
+    }
+
+    public static int getNumberInput(String message) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(message);
+        return in.nextInt();
+    }
+
+    public static int divisors(int num) {
+        int counter = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
