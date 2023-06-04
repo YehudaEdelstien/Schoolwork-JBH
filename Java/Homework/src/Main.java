0681f2827ba3b821ea28b958d88a29721f663595
YehudaEdelstien <72869192+YehudaEdelstien@users.noreply.github.com>
2023-06-04 15:32:42 +0300

    tidied up Main

--- Java/Homework/src/Main.java
+++ Java/Homework/src/Main.java
@@ -1,11 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] arr = new String[5];
-        System.out.println(arr[0]);
+        
     }
 }
-
-/*
-
-*/
