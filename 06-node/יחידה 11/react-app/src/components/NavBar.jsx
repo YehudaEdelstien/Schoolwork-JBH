ae2267c51883b75aebecc61f6aee301468bd45af
YehudaEdelstien <72869192+YehudaEdelstien@users.noreply.github.com>
2023-11-06 13:22:30 +0200

    href handler on navbar

--- 06-node/יחידה 11/react-app/src/components/NavBar.jsx
+++ 06-node/יחידה 11/react-app/src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import { useLocation, Link } from "react-router-dom"
 
-
 export default function About() {
   const location = useLocation().pathname
   const pathArr = location.split('/');
@@ -8,19 +7,27 @@ export default function About() {
   return (
     <div>
       {pathArr.length >= 1 && pathArr.map((el, index, arr) => {
-        if (el === '') return <>/</>
-        console.log(arr.length, index)
-        return (
-          <>
-            <Link to={el}>{el}</Link>
-            {index < arr.length - 1 && <>/</>}
-          </>
-        )
+        if (el === '' && index === 0) {
+          return <Link to={'./'}>root</Link>
+        }
+        
+        return <PathLink el={el} index={index} arr={arr}/>
       })}
     </div>
   )
 }
 
-function useHrefHandler(target) {
-  const location = useLocation().pathname;
+function PathLink({el, index, arr}) {
+  const path = useHrefHandler(index, arr)
+  console.log(path)
+  return <> / <Link to={path}>{el}</Link> </>
+}
+
+function useHrefHandler(index, arr) {
+  let location = '.';
+  for (let i = 1; i < index + 1; i++) {
+    location += '/' + arr[i];
+  }
+  console.log(location);
+  return location;
 }
